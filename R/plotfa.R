6325fe90ac55f20a4193202b8e9e3a6f764d4c0e
Diego Blum <blumworx@gmail.com>
2016-03-03 14:16:32 +0000

    version 1.1.0

--- R/plotfa.R
+++ R/plotfa.R
@@ -7,85 +7,85 @@
 #' The \code{plot_fa} function should not be assigned to an
 #' object, except when willing to use the optional arguments
 #' \code{switch.from} and \code{switch.to}. In the latter
-#' case, the target object should have the same name as the
+#' case, the object name should be the same as the name of the
 #' object of class \code{'fa_items'}, and argument
 #' \code{which} must designate the isomorph to be affected
 #' even if there is only one isomorph available. For example:
 #' \preformatted{object <- plot_fa(object, which = 1,
 #' switch.from = 1, switch.to = 2)}
 #'
-#' @param items An object of class \code{'fa_items'} generated with function \code{build_fa}.
-#' @param which A numeric vector designating which isomorph(s) to plot. Plot all by defect.
-#' @param mode A character string designating plot mode "A", "B" or "C". Plot mode "A" by defect.
-#' @param language A character string designating English ("E"), German ("D") or Spanish ("S") language. "E" by defect.
-#' @param language.dir A character string designating language for output files. "A" by defect selects all languages.
-#' @param form.int A character string designating the form from "A" to "D" of the internal shape lines, or "R" for random. "A" by defect.
-#' @param form.ext A character string designating the form from "A" to "D" of the external trapezium, or "R" for random. "A" by defect.
-#' @param size.shape A number designating the size of every shape. 1 by defect.
-#' @param size.dot A number designating the size of every shape dot. 2 by defect.
-#' @param size.line A number designating the thickness of every shape line. 1 by defect.
-#' @param size.q A number designating the size of the question mark. 3.5 by defect.
-#' @param size.word A number designating the size of the verbal options. 1.2 by defect.
-#' @param info Should rules applied and correct answers be informed? True by defect.
+#' @param items An object of class \code{'fa_items'} generated with function \code{build_fa}. No default.
+#' @param which A numeric vector designating which isomorph(s) to plot. Plot all by default.
+#' @param mode A character string designating plot mode "A", "B" or "C". Plot mode "A" by default.
+#' @param language A character string designating English ("E"), German ("D") or Spanish ("S") language. Default is "E".
+#' @param language.dir A character string designating language for output files. "A" by default selects all languages.
+#' @param form.int A character string designating the form from "A" to "D" of the internal shape lines, or "R" for random. Default is "A".
+#' @param form.ext A character string designating the form from "A" to "D" of the external trapezium, or "R" for random. Default is "A".
+#' @param size.shape A number designating the size of every shape. Default is 1.
+#' @param size.dot A number designating the size of every shape dot. Default is 2.
+#' @param size.line A number designating the thickness of every shape line. Default is 1.
+#' @param size.q A number designating the size of the question mark. Default is 3.5.
+#' @param size.word A number designating the size of the verbal options. Default is 1.2.
+#' @param info Should rules applied and correct answers be informed? True by default.
 #' @param directory Character string designating a file in your PC where to store the isomorphs.
 #' @param switch.from Number 'p' designating an option from 1 to 8 to switch with 'q'.
 #' @param switch.to Number 'q' designating an option from 1 to 8 to switch with 'p'.
-#' @return A data frame containing rules applied and right answers when \code{info=T} by defect, or an object of class \code{'fa_items'} when \code{which} has length 1, its value is greater than 0 and both \code{switch.from} and \code{switch.to} are greater than 0.
+#' @return A data frame containing rules applied and right answers when \code{info=T} by default, or an object of class \code{'fa_items'} when \code{which} has length 1, its value is greater than 0 and both \code{switch.from} and \code{switch.to} are greater than 0.
 #' @author Diego Blum \email{<blumworx@gmail.com>}
-#' @references Blum, D., Holling, H., Galibert, M.S., & Forthmann, B. (2016, in review). Task difficulty prediction of figural analogies. \emph{Intelligence}.
+#' @references Blum, D., Holling, H., Galibert, M.S., & Forthmann, B. (2016, in press). Task difficulty prediction of figural analogies. \emph{Intelligence}.
 #' @seealso \code{\link{IMak}}
 #' @examples
-#' \dontrun{Create two isomorphs with one rule, setting the correct answer to 1:}
+#' ## Create two isomorphs with one rule, setting the correct answer to 1:
 #' one <- build_fa(isomorphs = 2, dot.mov = c(1, 2), correct = 1)
-#' \dontrun{Plot them:}
+#' ## Plot them:
 #' plot_fa(one)
-#' \dontrun{Change the correct answer of item 2 from position 1 to position 2:}
+#' ## Change the correct answer of item 2 from position 1 to position 2:
 #' one <- plot_fa(one, which = 2, switch.from = 1, switch.to = 2)
-#' \dontrun{Choose a directory and save the items:
-#' dir1 <- "enter your new directory here"
-#' plot_fa(one, directory = dir1)}
+#' ## Choose a directory and save the items:
+#' # dir1 <- "enter your new directory here"
+#' # plot_fa(one, directory = dir1)
 #'
-#' \dontrun{Create four isomorphs with two rules:}
+#' ## Create four isomorphs with two rules:
 #' two <- build_fa(isomorphs = 4, mirror = 1, trap.rot = c(90, 45))
-#' \dontrun{Plot them in German language:}
-#' plot_fa(two,language = "D")
-#' \dontrun{Plot only items 2 and 3 in Spanish and choose form "B" for the internal lines:}
+#' ## Plot them in German language:
+#' plot_fa(two, language = "D")
+#' ## Plot only items 2 and 3 in Spanish and choose form "B" for the internal lines:
 #' plot_fa(two, language = "S", form.int = "B", which = c(2, 3))
-#' \dontrun{Choose a different directory and save these two items by keeping the latter configuration:
-#' dir2 <- "enter your new directory here"
-#' plot_fa(two, which = c(2, 3), language.dir = "S", form.int = "B", directory = dir2)}
+#' ## Choose a different directory and save these two items by keeping the latter configuration:
+#' # dir2 <- "enter your new directory here"
+#' # plot_fa(two, which = c(2, 3), language.dir = "S", form.int = "B", directory = dir2)
 #'
-#' \dontrun{Create 20 isomorphs with three rules. Set \code{automatic=F} and affect the options:}
+#' ## Create 20 isomorphs with three rules. Set automatic=F and affect the options:
 #' three <- build_fa(isomorphs = 20, mirror = 1, trap.rot = c(90, 45), dot.mov = c(1, 2),
 #' automatic = FALSE, al.mirror = c(0, 1), al.trap.rot = -45, al.dot.mov = 1)
-#' \dontrun{Plot them:}
+#' ## Plot them:
 #' plot_fa(three)
-#' \dontrun{Plot each individual shape of item 13 in German language only:}
+#' ## Plot each individual shape of item 13 in German language only:
 #' plot_fa(three, which = 13, mode = "C", language = "D")
-#' \dontrun{Choose a different directory and save the item parts:
-#' dir3 <- "enter your new directory here"
-#' plot_fa(three, which = 13, mode = "C", language.dir = "D", directory = dir3)}
+#' ## Choose a different directory and save the item parts:
+#' # dir3 <- "enter your new directory here"
+#' # plot_fa(three, which = 13, mode = "C", language.dir = "D", directory = dir3)
 #' @importFrom "grDevices" "dev.off" "png"
 #' @importFrom "graphics" "layout" "lines" "par" "plot" "points" "text"
 #' @importFrom "utils" "write.table"
 #' @export
 plot_fa <- function(
-  items,              #List containing items already built.
-  which = 0,          #0: plot all. Numbers > 0: item(s) chosen to ploy.
-  mode = "A",         #A, B, C: different plot types.
-  language = "E",     #E, D, S: English, Deutsch, Spanish.
-  language.dir = "A", #A: All languages are printed. E, D, S: A specific language is printed.
-  form.int = "A",     #A, B, C, D: different internal line types. R: random.
-  form.ext = "A",     #A, B, C, D: different trapezium types. R: random.
-  size.shape = 1,     #Size of every shape.
-  size.dot = 2,       #Size of the dot.
-  size.line = 1,      #Thickness of the lines.
-  size.q = 3.5,       #Size of the question mark.
-  size.word = 1.2,    #Size of options 9 and 10.
-  info = T,           #Print item information in directory.
-  directory = F,      #Choose a directory to save in png format.
-  switch.from = 0,    #Option p to be switched with q.
-  switch.to = 0)      #Option q to be switched with p.
+  items,
+  which = 0,
+  mode = "A",
+  language = "E",
+  language.dir = "A",
+  form.int = "A",
+  form.ext = "A",
+  size.shape = 1,
+  size.dot = 2,
+  size.line = 1,
+  size.q = 3.5,
+  size.word = 1.2,
+  info = T,
+  directory = F,
+  switch.from = 0,
+  switch.to = 0)
 
 {
 
@@ -103,30 +103,30 @@ plot_fa <- function(
   if ((switch.from > 0 | switch.to > 0) & (!switch.from %in% 1:8 | !switch.to %in% 1:8))
     stop("Incorrect data input for argument 'switch.from' or 'switch.to'.")
 
-  #BASIC CODE FOR FEATURES
+  # BASIC CODE FOR FEATURES
 
-  #Rotation function:
+  # Rotation function:
   rot <- function(x, angle=0) {
     cbind(cos(angle*0.0174532925)*x[,1] - sin(angle*0.0174532925)*x[,2],
     sin(angle*0.0174532925)*x[,1] + cos(angle*0.0174532925)*x[,2])
   }
 
-  #Mirroring function:
+  # Mirroring function:
   mir <- function(x) {
     cbind(-1*x[,1], x[,2])
   }
 
-  #Broken circle coordinates:
+  # Broken circle coordinates:
   t <- seq(0, 2*pi, length=200)
   bcirc <- cbind(3.5*sin(t[25:200]), 3.5*cos(t[25:200]))
 
-  #Number of isomorphs:
+  # Number of isomorphs:
   isomorphs <- length(items)
 
-  #a, b, c vector:
+  # a, b, c vector:
   abc <- c("a", "b", "c")
 
-  #Items to plot are all or some?
+  # Items to plot are all or some?
   all_or_some <- 0
   if (sum(which %in% 0:isomorphs) == length(which) &
         (sum(which > 0) == 0 | sum(which > 0) == length(which))) {
@@ -135,7 +135,7 @@ plot_fa <- function(
         "some"
   } else stop("Incorrect data imput for 'which'.")
 
-  #Creating a data frame with item characteristics:
+  # Creating a data frame with item characteristics:
   information <- data.frame(NA, NA, NA, NA, NA, NA)
   names(information) <- c("Isomorphs", "Correct", "      ",
                           "Rules", "      ", "Total")
@@ -186,24 +186,24 @@ plot_fa <- function(
       information[i,6] <- ""
     }
 
-  #PLOTTING FUNCTION
+  # PLOTTING FUNCTION
 
-  #Function to plot figures (it will be assigned to a list):
+  # Function to plot figures (it will be assigned to a list):
   plot.figure <- function(x) {
 
-    #Identify mirroring:
+    # Identify mirroring:
     bcirc_mir_or_not <- if (x$mirror == 0) bcirc else mir(bcirc)
     boot_mir_or_not <- if (x$mirror == 0) boot else mir(boot)
 
-    #Once mirroring is identified, apply rotation:
+    # Once mirroring is identified, apply rotation:
     rot_bcirc <- rot(bcirc_mir_or_not, angle=x$rotation)
     rot_boot <- rot(boot_mir_or_not, angle=x$rotation)
     rot_hammer <- rot(hammer, angle=x$hampos)
 
-    #How much to add when plotting in a different way?
+    # How much to add when plotting in a different way?
     increment <- c(0, 0, 0)
 
-    #Plot parameters:
+    # Plot parameters:
     if(directory != F & mode == "C") increment[1] <- 0.5
     if(directory == F & mode == "C") increment[1] <- 0.5
     limits <- 6-size.shape-increment[1]
@@ -211,7 +211,7 @@ plot_fa <- function(
     plot(xlim=c(-(limits), limits), ylim=c(-(limits), limits),
          0, type="l", bty="n", xaxt="n", yaxt="n", xlab="", ylab="")
 
-    #Drawing the plot:
+    # Drawing the plot:
     if(directory != F) {
       increment[2] <- 10
       increment[3] <- 23
@@ -232,13 +232,13 @@ plot_fa <- function(
     points(point, pch=16, cex=size.dot + increment[3])
     lines(rot_hammer, lwd=size.line + increment[2])}
 
-  #Setting the number of items to plot:
+  # Setting the number of items to plot:
   items_to_plot <- 0
   if (all_or_some == "all")
     items_to_plot <- 1:isomorphs else
     items_to_plot <- which
 
-  #Setting the directory (if any) to save the items and change language:
+  # Setting the directory (if any) to save the items and change language:
   if(directory != F) {
     setwd(directory)
     language <- if(language.dir == "A") 1:3 else
@@ -257,10 +257,10 @@ plot_fa <- function(
   }
   langname <- as.vector(langname)
 
-  #Properties of every item to be plotted:
+  # Properties of every item to be plotted:
   for (m in items_to_plot){
 
-    #Change places of options when choosing a single item:
+    # Change places of options when choosing a single item:
     if (length(which) == 1 & which[1] > 0 & switch.from %in% 1:8 &
         switch.to %in% 1:8 & directory == F) {
       if (switch.from == items[[which[1]]][[12]][[1]] |
@@ -300,9 +300,9 @@ plot_fa <- function(
                                  3, 4, 0, 10, 11, 12, 13, 14),
                                  2, 8, byrow=TRUE), widths=c(1.5, 1.5, 1, 1.5, 1.5, 1.5, 1.5, 1.5))},
               C = layout(matrix(1)))
-      par(mar = rep(1, 4)) #Shrink margins.
+      par(mar = rep(1, 4)) # Shrink margins.
 
-      #Correct plotting when plotting each figure separately:
+      # Correct plotting when plotting each figure separately:
       if (directory != F) {
         if (mode == "C") {
           plot.fa.size.q <- size.q + 30
@@ -320,10 +320,10 @@ plot_fa <- function(
             plot.fa.size.word <- size.word
           }
 
-      #Width of every single frame when mode is C:
+      # Width of every single frame when mode is C:
       width.single <- 800
 
-      #Internal boot coordinates subject to change:
+      # Internal boot coordinates subject to change:
       coord.x <- 3.5*sin(t[25])
       coord.y <- 3.5*cos(t[25])
       randomboot <- list(
@@ -342,7 +342,7 @@ plot_fa <- function(
                "C" = randomboot[[3]],
                "D" = randomboot[[4]])
 
-      #External trapezium (aka: hammer) coordinates subject to change:
+      # External trapezium (aka: hammer) coordinates subject to change:
       randomhammer <- list(
         A= cbind(c(1, -1, 0, 1, 1), c(3.5, 3.5, 4.5, 4.5, 3.5)),
         B= cbind(c(1, -1, -1, 0, 1), c(3.5, 3.5, 4.5, 4.5, 3.5)),
@@ -356,7 +356,7 @@ plot_fa <- function(
                "C" = randomhammer[[3]],
                "D" = randomhammer[[4]])
 
-      #Plot A, B & C:
+      # Plot A, B & C:
       for (i in 1:3) {
         if(directory != F & mode == "C")
           png(paste("item", m, abc[i], ".png", sep=""),
@@ -365,7 +365,7 @@ plot_fa <- function(
         if(directory != F & mode == "C") dev.off()
       }
 
-      #Plot question mark:
+      # Plot question mark:
       if(directory != F & mode == "C")
         png(paste("item", m, "d(q).png", sep=""),
             width = width.single, height=width.single)
@@ -375,7 +375,7 @@ plot_fa <- function(
       text(0, 0, "?", cex=plot.fa.size.q)
       if(directory != F & mode == "C") dev.off()
 
-      #Plot options:
+      # Plot options:
       for (i in 1:8){
         if(directory != F & mode == "C")
           png(paste("item", m, "op", i, ".png", sep=""), width = width.single, height=width.single)
@@ -383,7 +383,7 @@ plot_fa <- function(
         if(directory != F & mode == "C") dev.off()
       }
 
-      #Plot 9th and 10th options:
+      # Plot 9th and 10th options:
       op9 <- if(langname[h] == "D") "Keine\nAntwort\nist richtig" else
              if(langname[h] == "E") "No\nanswer\nis correct" else
              if(langname[h] == "S") "Ninguna\nrespuesta\nes correcta"
@@ -409,7 +409,7 @@ plot_fa <- function(
       if(directory != F & mode != "C") dev.off()
     }}
 
-  #Information about the items to be plotted:
+  # Information about the items to be plotted:
   if (directory != F & info == T)
     write.table(information, "Info.csv", quote=F, sep=";", row.names=F)
 
